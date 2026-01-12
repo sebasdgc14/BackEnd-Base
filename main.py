92c268ac59b14d7de803f8ec9e1e486842fb7116
sebasdgc14 <sebasdgc14@gmail.com>
2026-01-12 15:02:48 -0500

    base validation and schemas

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()  # This creates the app
 
 
-@app.get("/")
-def index():
-    return {"data": {"name": "Sebastian"}}
+@app.get("/blog")
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {"data": f"{limit} published blogs from the db"}
+    else:
+        return {"data": f"{limit} blogs from the db"}
 
 
-@app.get("/about")
-def about():
-    return {"data": "about page"}
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished blogs"}
+
+
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id: int):
+    return {"comments": "I like it"}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool] = False
+
+
+@app.post("/blog")
+def create_blog(request: Blog):
+    return {"data": f"Blog is created with name: {request.title}"}
