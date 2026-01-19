b8b1230b9e4b6f8b475c50e732a0eba724b50d76
sebasdgc14 <sebasdgc14@gmail.com>
2026-01-19 11:39:22 -0500

    Added blog login restriction via Oauth2

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,46 +1,15 @@
-from fastapi import Depends, status, APIRouter, HTTPException
-from .. import schemas, models, hashing
-from ..repository import blog, user
+from fastapi import Depends, APIRouter
 from ..database import get_db
 from sqlalchemy.orm import Session
-from typing import List
-
-
-from pydantic import BaseModel
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-
-# openssl rand -hex 32 to get a token
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from .. import schemas
+from ..repository import authentication
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(prefix="/login", tags=["Authentication"])
 
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: str
-
-
-# oauth2_scheme = OAuth2PasswordBearer(token_Url="token")
-
-
-def authenticate_user(request: schemas.login, db: Session = Depends(get_db)):
-    user_credentials = user.get_user_email(request.username, db)
-    verify_password = hashing.Hash.verify_password(
-        request.password, user_credentials.password
-    )
-    if not verify_password:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials"
-        )
-    return user_credentials
-
-
-@router.post("", response_model=schemas.UserBase)
-def login(request: schemas.login, db: Session = Depends(get_db)):
-    return authenticate_user(request, db)
+@router.post("", response_model=schemas.Token)
+def login(
+    request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    return authentication.authenticate_user(request, db)
--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -4,35 +4,56 @@ from ..repository import blog
 from ..database import get_db
 from sqlalchemy.orm import Session
 from typing import List
+from .. import oauth2
 
 router = APIRouter(prefix="/blog", tags=["blog"])
 
 
 # Get all blogs
-@router.get("", response_model=List[schemas.Blog])
-def get_all(db: Session = Depends(get_db)):
+@router.get("", response_model=List[schemas.ShowBlog])
+def get_all(
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
     return blog.get_all(db)
 
 
 # Get specific blog
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
-def get_blog(id, db: Session = Depends(get_db)):
+def get_blog(
+    id,
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
     return blog.get_blog(id, db)
 
 
 # Create a blog
 @router.post("", status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    return blog.create(request, db)
+def create(
+    request: schemas.Blog,
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
+    return blog.create(request, current_user.id, db)
 
 
 # Delete a blog
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_blog(id, db: Session = Depends(get_db)):
-    return blog.delete_blog(id, db)
+def delete_blog(
+    id,
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
+    return blog.delete_blog(id, current_user.id, db)
 
 
 # Update a blog
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    return blog.update_blog(id, request, db)
+def update_blog(
+    id,
+    request: schemas.Blog,
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
+    return blog.update_blog(id, current_user.id, request, db)
